c6d5a9493ef6c0436044fd0a5b09d99ecb2e5a4f
x0r <xorrezerv2@list.ru>
2018-03-19 12:36:19 +0300

    Compiled with ML 6.15

--- build_masm.bat
+++ build_masm.bat
@@ -1,5 +1,6 @@
 del iplay_masm.obj iplay_masm.exe iplay_m.exe
-JWasm.exe -Fl -Zd -Zi3 iplay_masm.asm 
+rem JWasm.exe -Fl -Zd -Zi3 iplay_masm.asm 
+ml.exe /c /Fl /Sa iplay_masm.asm 
 rem wlink  name iplay_nasm.exe system dos file iplay_nasm.obj option verbose
 alink -m -oEXE iplay_masm.obj
 \upx.exe  --ultra-brute iplay_masm.exe
