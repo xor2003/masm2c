d7b2ebf71e862da4a453dd4575f65ff59bfca5ec
x0r <xorrezerv2@list.ru>
2020-07-05 05:38:17 +0300

    Line continuation \. Label with spec chars. Misc

--- _masm61.py
+++ _masm61.py
@@ -8,22 +8,25 @@ grammar = Grammar.from_file(file_name, ignore_case=True)
 
 from parglare import Parser
 #parser = Parser(grammar, debug=True, debug_trace=True)
-#parser = Parser(grammar, debug=True)
-parser = Parser(grammar)
+parser = Parser(grammar, debug=True)
+#parser = Parser(grammar)
 
 codeset = 'cp437'
-if sys.version_info >= (3, 0):
-   f = open(sys.argv[1], "rt", encoding=codeset)
-else:
-   f = open(sys.argv[1], "rt")
 
 if sys.version_info[0] >= 3:
      sys.stdout.reconfigure(encoding='utf-8')
 
+for i in sys.argv[1:]:
 
-input_str=f.read()
+  if sys.version_info >= (3, 0):
+     f = open(i, "rt", encoding=codeset)
+  else:
+     f = open(i, "rt")
 
-result = parser.parse(input_str)
-f.close()
 
-print(result)
+  input_str=f.read()
+
+  result = parser.parse(input_str)
+  f.close()
+
+  print(result)
