750e8a25a2350b2c2f99945fb69a1bbed20e64f9
x0r <xorrezerv2@list.ru>
2021-01-28 16:12:56 +0300

    Trying to fix Travis #2

--- install-test.sh
+++ install-test.sh
@@ -2,4 +2,8 @@
 
 pip install --upgrade pip || exit 1
 pip install coverage pytest || exit 1
+
+git clone https://github.com/igordejanovic/parglare.git parglare
+pip install parglare
+
 pip install -e .[test] || exit 1
