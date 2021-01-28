e809175f8aa9997e2545ba57716f4e17c81a604a
x0r <xorrezerv2@list.ru>
2021-01-28 22:59:59 +0300

    Trying to fix Travis #6

--- install-test.sh
+++ install-test.sh
@@ -3,7 +3,7 @@
 pip install --upgrade pip
 pip install coverage pytest
 
-git clone https://github.com/igordejanovic/parglare.git parglare
-pip install parglare
+git clone https://github.com/igordejanovic/parglare.git parglaredev
+pip install parglaredev/
 
 pip install -e .[test]
