20ca8819a91f1b15878ea85f5d45b5cc650e6e52
x0r <xorrezerv2@list.ru>
2021-01-29 18:03:09 +0300

    Trying to add binary tests to Travis

--- asmTests/_config.sh
+++ asmTests/_config.sh
@@ -3,7 +3,7 @@
 #export CXX="g++"
 #SDL=$(pkg-config --cflags --libs sdl2)
 export SDL="-DNOSDL"
-export CURSES="$(pkg-config --cflags --libs ncurses)"
+export CURSES="$(pkg-config --cflags --libs ncurses) -L/lib/i386-linux-gnu/ -L/usr/lib/i386-linux-gnu/"
 export OPT="-m32 -Wno-narrowing -mno-ms-bitfields  -Wno-multichar $SDL $CURSES -D_GNU_SOURCE=1   -ggdb3 -O0 -I. -I.. -DDEBUG=3"
 #-D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600 -DCHTYPE_16 
 #  -mconsole -DSDL_MAIN_HANDLED
