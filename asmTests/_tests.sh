55cb943d85d95424f8c1b1cf70bd91ffc6869027
x0r <xorrezerv2@list.ru>
2021-03-09 19:09:09 +0300

    Fix tests error codes

--- asmTests/_tests.sh
+++ asmTests/_tests.sh
@@ -1,33 +1,34 @@
 #!/bin/bash -ex
 
+filename="$1"
+
 > _result.log
 
 . ./_config.sh
 
+(
 cd ..
 $CXX $OPT asm.cpp -c
 $CXX $OPT memmgr.cpp -c
-cd asmTests
+)
 
 result=0
 
-fail()
-{
-  echo "$n failed $1"
-  #exit $1
-  result=$(( $result + 1 ))
-  export result
-}
-
-ls *.asm | \
-while read name;
+for name in $(ls *.asm|xargs)
 do 
   n=$(echo $name| perl -pe 's!\.asm!!');
   export n
 #  (
 echo "Testing $n:"
-./_singletest.sh $n 2>&1 || fail $? 
-done | tee -a _result.log
+./_singletest.sh $n 2>&1 >> _result.log
+res=$?
+if [ $res -ne 0 ];then
+  echo "$n failed $res" >> _result.log
+  result=$(( $result + 1 ))
+  export result
+fi
+cat _result.log
+done
 
 echo "Total result: $result"
 exit $result
