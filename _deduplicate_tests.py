5b9405901a01b01cec99673f96f390609f2754b4
x0r <xorrezerv2@list.ru>
2021-05-22 22:22:38 +0300

    Add test's duplicate finder

--- _deduplicate_tests.py
+++ _deduplicate_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import re
 import sys
 
@@ -14,6 +14,9 @@ class FindDuplicateCoverage:
 
     def __init__(self):
         self.collected = []
+        self.name = None
+        self.cov = None
+        self.hash = None
 
     def pytest_collection_modifyitems(self, items):
         for item in items:
@@ -21,40 +24,49 @@ class FindDuplicateCoverage:
                 self.collected.append(item.name)
 
     def pytest_runtest_logstart(self, nodeid, location):
-        print(f"\nStart test {nodeid}")
+        #print(f"\nStart test {nodeid}")
         self.name = re.sub(r'^[^/]+/', '', nodeid)
         self.cov = Coverage(branch=True)
         self.hash = Hasher()
-        self.cov.start()
+        try:
+            self.cov.start()
+        except:
+            self.cov = None
 
     def pytest_runtest_logfinish(self, nodeid, location):
-        self.cov.stop()
-        print(f"\nStop test {nodeid}")
-        data = self.cov.get_data()
-        for f in data.measured_files():
-            # print(f)
-            # print(data.arcs(f))
-            add_data_to_hash(data, f, self.hash)
-        text_hash = self.hash.hexdigest()
+        if self.cov:
+            try:
+                self.cov.stop()
+                #print(f"\nStop test {nodeid}")
+                data = self.cov.get_data()
+                for f in data.measured_files():
+                    # print(f)
+                    # print(data.arcs(f))
+                    add_data_to_hash(data, f, self.hash)
+                text_hash = self.hash.hexdigest()
 
-        print(text_hash)
-        if text_hash in tests:
-            tests[text_hash] += self.name
-        else:
-            tests[text_hash] = [self.name]
+                #print(text_hash)
+                if text_hash in tests:
+                    tests[text_hash] += [self.name]
+                else:
+                    tests[text_hash] = [self.name]
 
-        self.cov.erase()
-        self.name = None
-        self.cov = None
-        self.hash = None
+                self.cov.erase()
+            except Exception as ex:
+                print("Exception %s", str(ex.args()))
+
+            self.name = None
+            self.cov = None
+            self.hash = None
 
 
 my_plugin = FindDuplicateCoverage()
 # directory = '.'
 pytest.main(sys.argv[1:], plugins=[my_plugin])
 
-for k, v in tests:
+print("\n\nDuplicates:")
+for k, v in tests.items():
     if len(v) > 1:
-        for i in v:
+        for i in sorted(v):
             print(i)
         print('\n')
