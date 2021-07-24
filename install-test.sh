8b2a7a9512cdd280566352fcca82d15d3fa01a94
x0r <xorrezerv2@list.ru>
2021-07-24 17:45:33 +0300

    fix: forgotten requirement

--- install-test.sh
+++ install-test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -exv
 
 pip install --upgrade pip
-pip install pytest coverage parglare
+pip install pytest coverage
+pip install -r requirements.txt
 
 #git clone https://github.com/igordejanovic/parglare.git parglaredev
 #pip install parglaredev/
