c7926560f6f1b45519b281717e0a24ba2355cbf3
x0r <xorrezerv2@list.ru>
2021-07-24 18:01:39 +0300

    fix: forgotten requirement

--- _binarytests.sh
+++ _binarytests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -ex
-cd asmTests
-
 #git clone https://github.com/igordejanovic/parglare.git parglaredev
 #pip install parglaredev/
 #rm -rf parglaredev/
 pip install -r requirements.txt
+
+cd asmTests
+
 
 ./_tests.sh 
