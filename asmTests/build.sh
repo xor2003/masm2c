8949956dce7cc19425b4ff36343a7ce84360a293
x0r <xorrezerv2@list.ru>
2021-08-05 16:51:39 +0300

    trying to fix: unittests

--- asmTests/build.sh
+++ asmTests/build.sh
@@ -4,6 +4,6 @@ set -ex
 #$CC $OPT $1.cpp -E >$1.e
 #$CXX $OPT $1.cpp -c
 #$CXX  $1.o ../asm.o ../memmgr.o  -o $1 $OPT
-$CXX _data.cpp $1.cpp ../asm.o ../memmgr.o $OPT -o $1
+$CXX _data.cpp $1.cpp ../asm.o ../memmgr.o $OPT -o $1 2>&1
 
 
