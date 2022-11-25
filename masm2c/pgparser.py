a6defdb89b21fed72a8aff3c82ba5d96ca3d075c
x0r <xor2003@gmx.com>
2022-11-25 19:22:10 +0300

    fix operations order

--- masm2c/pgparser.py
+++ masm2c/pgparser.py
@@ -369,7 +369,7 @@ class Asm2IR(Transformer):
     def register(self, children):
         # self.expression = self.expression or Expression()
         self.expression.element_size = self.context.is_register(children[0])
-        self.expression.registers.add(children[0])
+        self.expression.registers.add(children[0].lower())
         return children[0]  # Token('segmentregister', nodes[0].lower())
 
     def segmentregister(self, children):
