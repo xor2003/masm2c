35f4fcd28a1d8a028de68b658a243e7ffcf27083
x0r <xor2003@gmx.com>
2023-03-05 13:21:04 +0300

    fix c++ tests

--- asmTests/_singletest.sh
+++ asmTests/_singletest.sh
@@ -3,39 +3,40 @@ set -x
 if [ -z "$CXX" ];then
   export CXX=g++
 fi
-rm $1.cpp $1.h $1.err $1.log $1.txt $1.asm.log $1.o $1 2>/dev/null || true
+name="$1"
+n="$(echo $name| perl -pe 's!(\.asm|\.lst)!!')"
+
+rm $n.cpp $n.h $n.err $n.log $n.txt $n.asm.log $n.o $n 2>/dev/null || true
 ##./ml /c /Fl $1.asm
 #uasm /c /Fl $1.asm
-if [ -r "$1.asm" ];then
-  name="$1.asm"
-elif [ -r "$1.lst" ];then
-  name="$1.lst"
-elif [ -z "$1" ];then
+if [ -z "$name" ];then
   exit 0
-else
-  echo "No such file $1"
+elif [ ! -r "$name" ];then
+  echo "No such file $name"
   exit 2
 fi
 
-../masm2c.py -m separate "$name" > "$1.txt" 2>&1
+
+../masm2c.py -m separate "$name" > "$n.txt" 2>&1
 res=$?
 if [ "${res}" -ne 0 ];then
-   cat "$1.txt"
+   cat "$n.txt"
    exit "${res}"
 fi  
 
-./build.sh $1 > "$1.txt" 2>&1
+
+./build.sh $n > "$n.txt" 2>&1
 res=$?
 if [ "${res}" -ne 0 ];then
-   cat "$1.txt" asm.log
+   cat "$n.txt" asm.log
    exit "${res}"
 fi  
 rm asm.log || true
 
-./$1 > "$1.txt" 2>&1
+./$n > "$n.txt" 2>&1
 res=$?
 if [ "${res}" -ne 0 ];then
-   cat "$1.txt"
+   cat "$n.txt"
    exit "${res}"
 fi  
 #python ../_masm61.py  $1.asm
--- asmTests/_tests.sh
+++ asmTests/_tests.sh
@@ -21,11 +21,8 @@ result=0
 
 for name in $(ls *.asm *.lst|xargs)
 do 
-  n="$(echo $name| perl -pe 's!\.asm!!')"
-  export n
-#  (
-echo "Testing $n:"
-./_singletest.sh "$n" 2>&1 | tee -a _result.log
+  echo "Testing $name:"
+  ./_singletest.sh "$name" 2>&1 | tee -a _result.log
 res=${PIPESTATUS[0]}
 if [ $res -ne 0 ];then
   echo "$n failed with code $res" | tee -a _result.log
