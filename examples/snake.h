23fedfdd8fed74813c4de7a28be493b9f4dc582e
x0r <xorrezerv2@list.ru>
2023-06-04 20:03:15 +0300

    added hack for popf

--- examples/snake.h
+++ examples/snake.h
@@ -1,94 +1,100 @@
-#ifndef TASMRECOVER_SNAKE_STUBS_H__
-#define TASMRECOVER_SNAKE_STUBS_H__
+/* THIS IS GENERATED FILE */
 
-/* PLEASE DO NOT MODIFY THIS FILE. ALL CHANGES WILL BE LOST! LOOK FOR README FOR DETAILS */
-
-/* 
- *
- */
+        
+#ifndef __M2C_SNAKE_STUBS_H__
+#define __M2C_SNAKE_STUBS_H__
 
 #include "asm.h"
 
-//namespace snake {
 
-#define kbegin 0x1001
-#define kmain 0x1111
-#define kmainloop 0x1112
-#define kgameover_mainloop 0x1113
-#define kquitpressed_mainloop 0x1114
-#define kquit_mainloop 0x1115
-#define kdelay 0x1116
-#define kjmp_delay 0x1117
-#define kfruitgeneration 0x1118
-#define kregenerate 0x1119
-#define knevermind 0x111a
-#define kret_fruitactive 0x111b
-#define kdispdigit 0x111c
-#define kdispnum 0x111d
-#define kretz 0x111e
-#define ksetcursorpos 0x111f
-#define kdraw 0x1120
-#define kdraw_loop 0x1121
-#define kout_draw 0x1122
-#define kreadchar 0x1123
-#define kkeybdpressed 0x1124
-#define kkeyboardfunctions 0x1125
-#define knext_11 0x1126
-#define knext_12 0x1127
-#define knext_13 0x1128
-#define knext_14 0x1129
-#define kquit_keyboardfunctions 0x112a
-#define kshiftsnake 0x112b
-#define kl 0x112c
-#define koutside 0x112d
-#define knext_1 0x112e
-#define knext_2 0x112f
-#define knext_3 0x1130
-#define kdone_checking_the_head 0x1131
-#define kgame_over 0x1132
-#define ki_ate_fruit 0x1133
-#define kprintbox 0x1134
-#define kl1 0x1135
-#define kl2 0x1136
-#define kl3 0x1137
-#define kl4 0x1138
-#define kwritecharat 0x1139
-#define kreadcharat 0x113a
-#define kwritestringat 0x113b
-#define kloop_writestringat 0x113c
-#define kexit_writestringat 0x113d
 
-struct MYPACKED Memory{
- db dummy1[4096]; // protective
- db data[16]; // segment data
-char msg[28];
-char instructions[92];
-char quitmsg[37];
-char gameovermsg[28];
-char scoremsg[8];
-char head[3];
-char body[48];
-db segmentcount;
-db fruitactive;
-db fruitx;
-db fruity;
-db gameover;
-db quit;
-db delaytime;
- db dummy2[5]; // padding
- db text[16]; // segment text
+extern db& default_seg;
+extern db& data;
+extern char (& msg)[28];
+extern char (& instructions)[92];
+extern char (& quitmsg)[37];
+extern char (& gameovermsg)[28];
+extern char (& scoremsg)[8];
+extern char (& head)[3];
+extern db (& body)[48];
+extern db& segmentcount;
+extern db& fruitactive;
+extern db& fruitx;
+extern db& fruity;
+extern db& gameover;
+extern db& quit;
+extern db& delaytime;
+extern db& text;
 
-                        db stack[STACK_SIZE];
-                        db heap[HEAP_SIZE];
-                };
+namespace m2c{
+void   Initializer();
+static const dd kbegin = 0x1001;
+static const dd kmainproc = 0x1002;
+static const dd kmain = 0x1003;
+static const dd kmainloop = 0x1004;
+static const dd kgameover_mainloop = 0x1005;
+static const dd kquitpressed_mainloop = 0x1006;
+static const dd kquit_mainloop = 0x1007;
+static const dd kdelay = 0x1008;
+static const dd kjmp_delay = 0x1009;
+static const dd kfruitgeneration = 0x100a;
+static const dd kregenerate = 0x100b;
+static const dd knevermind = 0x100c;
+static const dd kret_fruitactive = 0x100d;
+static const dd kdispdigit = 0x100e;
+static const dd kdispnum = 0x100f;
+static const dd kretz = 0x1010;
+static const dd ksetcursorpos = 0x1011;
+static const dd kdraw = 0x1012;
+static const dd kdraw_loop = 0x1013;
+static const dd kout_draw = 0x1014;
+static const dd kreadchar = 0x1015;
+static const dd kkeybdpressed = 0x1016;
+static const dd kkeyboardfunctions = 0x1017;
+static const dd knext_11 = 0x1018;
+static const dd knext_12 = 0x1019;
+static const dd knext_13 = 0x101a;
+static const dd knext_14 = 0x101b;
+static const dd kquit_keyboardfunctions = 0x101c;
+static const dd kshiftsnake = 0x101d;
+static const dd kl = 0x101e;
+static const dd koutside = 0x101f;
+static const dd knext_1 = 0x1020;
+static const dd knext_2 = 0x1021;
+static const dd knext_3 = 0x1022;
+static const dd kdone_checking_the_head = 0x1023;
+static const dd kgame_over = 0x1024;
+static const dd ki_ate_fruit = 0x1025;
+static const dd kprintbox = 0x1026;
+static const dd kl1 = 0x1027;
+static const dd kl2 = 0x1028;
+static const dd kl3 = 0x1029;
+static const dd kl4 = 0x102a;
+static const dd kwritecharat = 0x102b;
+static const dd kreadcharat = 0x102c;
+static const dd kwritestringat = 0x102d;
+static const dd kloop_writestringat = 0x102e;
+static const dd kexit_writestringat = 0x102f;
+}
 
-//class snakeContext {
-//public:
-//      snakeContext() {}
+bool delay(m2c::_offsets, struct m2c::_STATE*);
+bool dispdigit(m2c::_offsets, struct m2c::_STATE*);
+bool dispnum(m2c::_offsets, struct m2c::_STATE*);
+bool draw(m2c::_offsets, struct m2c::_STATE*);
+bool fruitgeneration(m2c::_offsets, struct m2c::_STATE*);
+bool keyboardfunctions(m2c::_offsets, struct m2c::_STATE*);
+bool asmmain(m2c::_offsets, struct m2c::_STATE*);
+static bool mainproc(m2c::_offsets, struct m2c::_STATE*);
+bool printbox(m2c::_offsets, struct m2c::_STATE*);
+bool readchar(m2c::_offsets, struct m2c::_STATE*);
+bool readcharat(m2c::_offsets, struct m2c::_STATE*);
+bool setcursorpos(m2c::_offsets, struct m2c::_STATE*);
+bool shiftsnake(m2c::_offsets, struct m2c::_STATE*);
+bool writecharat(m2c::_offsets, struct m2c::_STATE*);
+bool writestringat(m2c::_offsets, struct m2c::_STATE*);
 
-//      void _start();
-//};
+bool __dispatch_call(m2c::_offsets __disp, struct m2c::_STATE* _state);
 
-//} // End of namespace DreamGen
+
 
 #endif
