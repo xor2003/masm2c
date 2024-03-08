dc5d58bb12b338725cb47500a5458b4c30102655
x0r <xorrezerv2@list.ru>
2024-03-08 23:49:41 +0100

    fix python 3.12 support

--- masm2c/__init__.py
+++ masm2c/__init__.py
@@ -1,10 +1,2 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "0.9.8"
-finally:
-    del get_distribution, DistributionNotFound
+dist_name = __name__
+__version__ = "0.9.8"
