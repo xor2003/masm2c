7ad82ff33474690e51266092da5fe05ad3e1ad35
x0r <xorrezerv2@list.ru>
2026-05-30 19:40:21 +0200

    chore: remove obsolete test wrapper scripts

--- runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-# Run all tests and generate coverage report
-
-coverage run --source masm2c -m pytest tests/
-#coverage report --fail-under 90 || exit 1
-# Run this to generate html report
-# coverage html --directory=coverage
-#flake8 || exit 1
